f96134cd23b5c0825e4941e751546f1b4ce56c5a
Diogo Biazus <diogob@gmail.com>
2016-05-27 22:57:33 -0400

    Implement search box and add some bootstrap CSS

--- src/ElmWorkshop.elm
+++ src/ElmWorkshop.elm
@@ -1,15 +1,20 @@
-module ElmWorkshop exposing (..)
+module ElmWorkshop exposing (Model, Msg, main, view, update)
 
 import Html.App as App
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (href, placeholder, class, target)
 import Html.Events exposing (..)
+
+import Json.Encode as Encode
+import Json.Decode exposing (Decoder, string, list, int)
+import Json.Decode.Pipeline exposing (decode, required, optional)
+import Task
+
+import HttpBuilder exposing(..)
 
 type Msg
   = SearchPackages String
-  | FetchPackages (Result String Model)
-  | ShowError String
-  | NoOp
+  | FetchPackages (Result String Packages)
 
 type alias Package =
   { package_name  : String
@@ -31,15 +36,17 @@ type alias Package =
   , updated_at    : String
   }
 
+type alias Packages = List Package
+
 type alias Model =
-  { packages : List Package
-  , error : String
+  { packages : Packages
+  , error : Maybe String
   }
 
 main : Program Never
 main =
   App.program
-    { init = ({packages = [], error = ""}, Cmd.none)
+    { init = ({packages = [], error = Nothing}, Cmd.none)
     , update = update
     , view = view
     , subscriptions = \_ -> Sub.none
@@ -48,11 +55,12 @@ main =
 view : Model -> Html Msg
 view model =
   div
-    []
+    [ class "container" ]
     [ topBar
     , Html.main'
-        [ class "app-body" ]
+        []
         [ searchView
+        , errorAlert model
         , packagesView model.packages
         ]
     ]
@@ -60,39 +68,84 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
   case (Debug.log "action" action) of
-    _ -> ( model, Cmd.none )
+    SearchPackages query ->
+      ( model, searchPackages query )
+    FetchPackages result ->
+      case result of
+        Ok pkgs ->
+          ( { packages = pkgs, error = Nothing }, Cmd.none )
+        Err errorMsg ->
+          ( { model | error = Just (Debug.log "err" errorMsg) }, Cmd.none )
 
 -- private functions
+apiUrl : String -> String
+apiUrl = (++) "http://localhost:3000"
 
-packagesView : List Package -> Html Msg
-packagesView model =
-  ul
-    []
-    (List.map packageView model)
+searchUrl : String
+searchUrl = apiUrl "/rpc/package_search"
+
+searchPackages : String -> Cmd Msg
+searchPackages query =
+  let
+    body = Encode.object
+      [ ("query", Encode.string query) ]
+  in
+    post searchUrl
+      |> withHeaders [("Content-Type", "application/json"), ("Accept", "application/json")]
+      |> withJsonBody body
+      |> (send (jsonReader decodeModel) stringReader)
+      |> Task.perform toError toOk
+
+toError : a -> Msg
+toError _ = FetchPackages (Err "There was an error in our search API, please try again later")
+
+toOk : Response Packages -> Msg
+toOk r = FetchPackages (Ok r.data)
+
+errorAlert : Model -> Html msg
+errorAlert model =
+  case model.error of
+    Nothing -> br [] []
+    Just msg ->
+      div [ class "alert alert-danger" ]
+            [ text msg
+            ]
 
 searchView : Html Msg
 searchView =
-   div []
-    [ input
-        [ placeholder "Search package"
-        , onInput SearchPackages
+   div [ class "row" ]
+    [ div [ class "col-lg-12" ]
+        [ input
+            [ placeholder "Search package"
+            , onInput SearchPackages
+            , class "form-control"
+            ]
+            []
         ]
-        []
     ]
 
 topBar : Html msg
 topBar =
   header
-    [ class "top-bar" ]
-    [ h1 [] [text "Elm Workshop"] ]
+    [ class "page-header" ]
+    [ h1 [] [ text "Elm Workshop" ] ]
 
 separator : Html msg
 separator = text " - "
 
+packagesView : Packages -> Html Msg
+packagesView model =
+  div [ class "row" ]
+        [ div [ class "col-lg-12" ]
+            [ ul [ class "list-group" ]
+                (List.map packageView model)
+            ]
+        ]
+
 packageView : Package -> Html Msg
 packageView model =
   li
-    []
+    [ class "list-group-item" ]
     [ p
       []
       [ h4 [] [ text model.package_name ]
@@ -123,3 +176,25 @@ packageView model =
         ]
       ]
     ]
+
+decodeModel : Decoder Packages
+decodeModel =
+  decode Package
+    |> required "package_name" string
+    |> required "version" string
+    |> required "license" string
+    |> required "description" string
+    |> required "category" string
+    |> required "homepage" string
+    |> required "package_url" string
+    |> required "repo_type" string
+    |> required "repo_location" string
+    |> required "stars" int
+    |> required "forks" int
+    |> required "collaborators" int
+    |> required "extensions" (list string)
+    |> required "dependencies" (list string)
+    |> required "dependents" (list string)
+    |> required "created_at" string
+    |> required "updated_at" string
+    |> list
