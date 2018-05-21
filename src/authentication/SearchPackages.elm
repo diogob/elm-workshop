703d51207f2a016e607951e2d910f4d80ad0b92a
Diogo Biazus <diogo@biazus.me>
2018-05-20 20:19:08 -0400

    Copy complete search solution to authentication chapter.

---
+++ src/authentication/SearchPackages.elm
@@ -0,0 +1,233 @@
+module ElmWorkshop exposing (Model, Msg, main, view, update)
+
+import Html as App
+import Html exposing (..)
+import Html.Attributes exposing (href, placeholder, class, target)
+import Html.Events exposing (..)
+
+import Http exposing (Error)
+import Json.Encode as Encode
+import Json.Decode exposing (Decoder, succeed, string, list, int, at, field)
+import Json.Decode.Extra exposing (..)
+import Debug exposing (log)
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Grid.Row as Row
+import Bootstrap.Button as Button
+import Bootstrap.Form.Input as Input
+import Bootstrap.Form.InputGroup as InputGroup
+import Bootstrap.Utilities.Spacing as Sp
+
+import HttpBuilder exposing(..)
+
+type Msg
+  = FetchPackages Int
+  | RenderPackages Packages
+  | Error String
+  | QueryInput String
+  | SendQuery
+
+type alias Package =
+  { package_name  : String
+  , version       : String
+  , license       : String
+  , description   : String
+  , category      : String
+  , homepage      : String
+  , package_url   : String
+  , repo_type     : String
+  , repo_location : String
+  , stars         : Int
+  , forks         : Int
+  , collaborators : Int
+  , extensions    : List String
+  , dependencies  : List String
+  , dependents  : List String
+  , created_at    : String
+  , updated_at    : String
+}
+
+type alias Packages = List Package
+
+type alias Model =
+  { packages : Packages
+  , error : Maybe String
+  , query : Maybe String
+  }
+
+main : Program Never Model Msg
+main =
+  App.program
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = \_ -> Sub.none
+    }
+
+init : ( Model, Cmd Msg )
+init = ({packages = [], error = Nothing, query = Nothing}, Cmd.none)
+
+
+view : Model -> Html Msg
+view model =
+    Grid.container []
+        [ CDN.stylesheet
+        , Grid.row [Row.attrs [Sp.mt3]] [
+               Grid.col [] [
+                    h2 [] [text "Elm Workshop"]
+                    ]
+               ]
+        , Grid.row [Row.attrs [Sp.mt3]] [
+            Grid.col [ Col.lg6 ]
+            [ InputGroup.config
+                ( InputGroup.text [Input.onInput QueryInput, Input.placeholder "Search for" ] )
+                |> InputGroup.successors
+                    [ InputGroup.button [ Button.onClick SendQuery, Button.secondary ] [ text "Search"] ]
+                |> InputGroup.view
+            ]
+        ]
+        , Grid.row [Row.attrs [Sp.mt3]] [
+               Grid.col [] [
+                    packagesView model.packages
+                   ]
+              ]
+        ]
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update action model =
+  case action of
+    RenderPackages packages ->
+        ({model | packages = packages}, Cmd.none)
+    QueryInput query ->
+        ({ model | query = Just query }, Cmd.none)
+    SendQuery ->
+        ( model
+        , case model.query of
+              Nothing -> Cmd.none
+              Just query -> searchPackages query
+        )
+    _ ->
+        (model, Cmd.none)
+
+-- private functions
+apiUrl : String -> String
+apiUrl = (++) "http://localhost:3000"
+
+packagesUrl : String
+packagesUrl = apiUrl "/packages"
+
+searchPackagesUrl : String
+searchPackagesUrl = apiUrl "/rpc/search_packages"
+
+errorAlert : Model -> Html msg
+errorAlert model =
+  case model.error of
+    Nothing -> br [] []
+    Just msg ->
+      div [ class "alert alert-danger" ]
+            [ text msg
+            ]
+
+separator : Html msg
+separator = text " - "
+
+packagesView : Packages -> Html Msg
+packagesView model =
+  Grid.row []
+        [ Grid.col []
+            [ ul [ class "list-group" ]
+                (List.map packageView model)
+            ]
+        ]
+
+packageView : Package -> Html Msg
+packageView model =
+  li
+    [ class "list-group-item" ]
+    [ p
+      []
+      [ h4 [] [ text model.package_name ]
+      , ul
+        []
+        [ li [] [text model.description]
+        , li [] [text ("Category: " ++ model.category)]
+        , li []
+          [ a
+            [ href ("http://hackage.haskell.org/package/" ++ model.package_name), target "blank" ]
+            [ text "Hackage" ]
+          , separator
+          , a
+            [ href model.repo_location, target "blank" ]
+            [ text "Source code" ]
+          ]
+        , li []
+          [ text ((toString (List.length model.dependencies)) ++ " Dependencies")
+          , separator
+          , text ((toString (List.length model.dependents)) ++ " Dependents")
+          , separator
+          , text ((toString model.stars) ++ " Stars")
+          , separator
+          , text ((toString model.forks) ++ " Forks")
+          , separator
+          , text ((toString model.collaborators) ++ " Collaborators")
+          ]
+        ]
+      ]
+    ]
+
+decodePackages : Decoder Packages
+decodePackages =
+  list (
+    succeed Package
+      |: (field "package_name" string)
+      |: (field "version" string)
+      |: (field "license" string)
+      |: (field "description" string)
+      |: (field "category" string)
+      |: (field "homepage" string)
+      |: (field "package_url" string)
+      |: (field "repo_type" string)
+      |: (field "repo_location" string)
+      |: (field "stars" int)
+      |: (field "forks" int)
+      |: (field "collaborators" int)
+      |: (field "extensions" (list string))
+      |: (field "dependencies" (list string))
+      |: (field "dependents" (list string))
+      |: (field "created_at" string)
+      |: (field "updated_at" string)
+  )
+
+searchPackages : String -> Cmd Msg
+searchPackages query =
+    let
+        body = Encode.object
+               [ ("query", Encode.string query) ]
+    in
+        post searchPackagesUrl
+            |> withHeaders [("Accept", "application/json")]
+            |> withHeader "Range" "0-99"
+            |> withJsonBody body
+            |> withExpect (Http.expectJson decodePackages)
+            |> send renderPackages
+
+getPackages : List (String, String) -> Cmd Msg
+getPackages query =
+    get packagesUrl
+        |> withQueryParams query
+        |> withHeader "Range" "0-9"
+        |> withExpect (Http.expectJson decodePackages)
+        |> send renderPackages
+
+getLimitPackages : Cmd Msg
+getLimitPackages = getPackages [("limit", "10")]
+
+renderPackages : Result Error Packages -> Msg
+renderPackages r =
+  case r of
+    Err httpError -> toError httpError
+    Ok packages -> RenderPackages packages
+
+toError : a -> Msg
+toError _ = Error "API Error"
